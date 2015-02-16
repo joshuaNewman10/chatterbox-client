aad52db715321b875f8151c7873addec9f5f9543
Hack Reactor Students <accounts@hackreactor.com>
2015-02-16 15:08:29 -0800

    Set up enviroment.  Completed Ajax get request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,16 @@
 // YOUR CODE HERE:
 
+$.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  data: JSON,
+  contentType: 'application/json',
+  success: function (data) {
+    console.log(data);
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to get message');
+  }
+});
